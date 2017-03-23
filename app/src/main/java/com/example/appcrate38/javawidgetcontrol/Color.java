02fb5c5bf81f8e17e0ccfde52ce67f384fdab6d9
appcrate38 <appcrate38@appcrate38ui-iMac.local>
2017-03-23 20:08:36 +0900

    #2 change widget in the java

---
+++ app/src/main/java/com/example/appcrate38/javawidgetcontrol/Color.java
@@ -0,0 +1,14 @@
+package com.example.appcrate38.javawidgetcontrol;
+
+/**
+ * Created by appcrate38 on 2017. 3. 23..
+ */
+
+class Color {
+    public static int rgb(int i, int i1, int i2) {
+        return 0;
+    }
+
+    public class WHITE {
+    }
+}
--- app/src/main/java/com/example/appcrate38/javawidgetcontrol/MainActivity.java
+++ app/src/main/java/com/example/appcrate38/javawidgetcontrol/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.appcrate38.javawidgetcontrol;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text1=(TextView) findViewById(R.id.text1);
+        TextView text2=(TextView) findViewById(R.id.text2);
+        TextView text3=(TextView) findViewById(R.id.text3);
+        TextView text4=(TextView) findViewById(R.id.text4);
+
+        text1.setText("First TextView");
+        text1.setTextSize(30);
+        text1.setBackgroundColor(Color.rgb(255,0,255));
+        text1.setTextColor(Color.WHITE);
+
+        text2.setText(R.string.text2_str);
+        text1.setText("Second TextView");
+        text2.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC);
+
+        text3.setTextColor(Color.GREEN);
+        text1.setText("Third TextView");
+        text3.setTextSize(15);
+
+        text4.setTextColor(Color.GRAY);
+        text1.setText("Fourth TextView");
+        text4.setTextSize(30);
     }
 }
